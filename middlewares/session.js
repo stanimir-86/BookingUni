1253a5631d988db966c8f90e1ddcd9cef87e6653
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-06 20:03:49 +0300

    Create session.js

---
+++ middlewares/session.js
@@ -0,0 +1,20 @@
+const { verifyToken } = require("../services/userService.js");
+
+module.exports = () => (req, res, next) => {
+
+    const token = req.cookies.token;
+
+    if (token) {
+        try {
+            const userData = verifyToken(token);
+            req.user = userData;
+        } catch (err) {
+            res.clearCookie('token');
+            res.redirect('/auth/login');
+            return;
+        }
+    }
+
+    next();
+
+};
