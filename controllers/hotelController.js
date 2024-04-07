e7e660a90d078475e869382c66e3eb2564027e6c
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-07 13:10:09 +0300

    Update hotelController.js

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,3 +1,7 @@
+const { create } = require('../services/hotelService.js');
+const { parseError } = require('../util/parser.js');
+
+
 const hotelCotntroller = require('express').Router();
 
 hotelCotntroller.get('/:id/details', (req, res) => {
@@ -10,6 +14,31 @@ hotelCotntroller.get('/create', (req, res) => {
     res.render('create', {
         title: 'Create Hotel'
     })
+});
+
+hotelCotntroller.post('/create', async (req, res) => {
+    const hotel = {
+        name: req.body.name,
+        city: req.body.city,
+        imageUrl: req.body.imageUrl,
+        rooms: Number(req.body.rooms),
+        owner: req.user._id,
+    }
+
+
+    try {
+        if (Object.values(hotel).some(v => !v)) {
+            throw new Error('All fields are required');
+        }
+        await create(hotel);
+        res.redirect('/');
+    } catch (err) {
+        res.render('create', {
+            title: 'Create Hotel',
+            body: hotel,
+            errors: parseError(err)
+        })
+    }
 });
 
 hotelCotntroller.get('/:id/edit', (req, res) => {
