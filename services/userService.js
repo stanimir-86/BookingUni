9e3b287939947611cc76daa8f342583079906f87
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-07 12:08:06 +0300

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -4,41 +4,46 @@ const User = require('../models/User.js');
 
 const JWT_SECRET = 'sfgjdfjdrtrdthdfg';
 
-async function register(username, password) {
+async function register(email, username, password) {
 
-    const existing = await User.findOne({ username }).collation({ locale: 'en', strength: 2 });
-    if (existing) {
+    const existingUsername = await User.findOne({ username }).collation({ locale: 'en', strength: 2 });
+    if (existingUsername) {
         throw new Error('Username is taken');
     }
-
+    const existingEmail = await User.findOne({ email }).collation({ locale: 'en', strength: 2 });
+    if (existingEmail) {
+        throw new Error('Email is taken');
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
+        email,
         username,
         hashedPassword
     });
-    //TODO see asigments if registration creates user session
+
     return createSession(user);
 
 }
 
-async function login(username, password) {
-    const user = await User.findOne({ username }).collation({ locale: 'en', strength: 2 })
+async function login(email, password) {
+    const user = await User.findOne({ email }).collation({ locale: 'en', strength: 2 })
     if (!user) {
-        throw new Error("Incorect user or password");
+        throw new Error("Incorect email or password");
     }
     const hasMatch = await bcrypt.compare(password, user.hashedPassword);
     if (hasMatch == false) {
-        throw new Error("Incorect user or password");
+        throw new Error("Incorect email or password");
 
     }
 
     return createSession(user);
 
 }
-function createSession({ _id, username }) {
+function createSession({ _id, email, username }) {
     const payload = {
         _id,
+        email,
         username
     };
 
