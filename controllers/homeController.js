688fe440806cb3f7ad796979937c51e5676ae0f3
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-07 13:10:07 +0300

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,13 @@
+const { getAll } = require('../services/hotelService.js');
+
 const homeController = require('express').Router();
 
 
-homeController.get('/', (req, res) => {
+homeController.get('/', async (req, res) => {
+    const hotels = await getAll()
     res.render('home', {
         title: 'Home Page',
-        user: req.user,
+       hotels
     });
 });
 
