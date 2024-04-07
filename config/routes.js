c45826240934a20a92977481448fa2e2cf22d803
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-07 13:10:05 +0300

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -2,11 +2,12 @@ const authController = require("../controllers/authController.js");
 const homeController = require("../controllers/homeController.js");
 const hotelCotntroller = require("../controllers/hotelController.js");
 const profileController = require("../controllers/profileController.js");
+const { hasUser } = require("../middlewares/guards.js");
 
 module.exports = (app) => {
 
     app.use('/', homeController);
     app.use('/auth', authController);
-    app.use('/hotel', hotelCotntroller);
+    app.use('/hotel', hasUser(), hotelCotntroller);
     app.use('/profile', profileController)
 };
