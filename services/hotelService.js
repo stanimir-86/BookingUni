d9966274b3d05d65f0fd24d983f79b5481160d63
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-08 11:34:02 +0300

    Update hotelService.js

--- services/hotelService.js
+++ services/hotelService.js
@@ -31,9 +31,7 @@ async function deleteById(id) {
 async function bookRoom(hotelId, userId) {
     const hotel = await Hotel.findById(hotelId);
 
-    if (hotel.bookings.includes(userId)) {
-        throw new Error('Cannot book twice');
-    }
+   
 
     hotel.bookings.push(userId);
     await hotel.save();
