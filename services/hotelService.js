c33e01eac7a5e05c80791ba093d9cc57f5731973
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-08 11:28:07 +0300

    Update hotelService.js

--- services/hotelService.js
+++ services/hotelService.js
@@ -9,15 +9,34 @@ async function getById(id) {
 async function create(hotel) {
     return await Hotel.create(hotel)
 };
+async function getByUserBooking(userId) {
+    return Hotel.find({ bookings: userId }).lean();
+}
+
 async function update(id, hotel) {
+    const existing = await Hotel.findById(id);
+
+    existing.name = hotel.name;
+    existing.city = hotel.city;
+    existing.rooms = hotel.rooms;
+    existing.imageUrl = hotel.imageUrl;
+
+    await existing.save();
 
 };
 async function deleteById(id) {
-
+    await Hotel.findByIdAndDelete(id)
 };
 
 async function bookRoom(hotelId, userId) {
+    const hotel = await Hotel.findById(hotelId);
 
+    if (hotel.bookings.includes(userId)) {
+        throw new Error('Cannot book twice');
+    }
+
+    hotel.bookings.push(userId);
+    await hotel.save();
 }
 
 module.exports = {
@@ -26,5 +45,6 @@ module.exports = {
     create,
     update,
     deleteById,
-    bookRoom
+    bookRoom,
+    getByUserBooking,
 }
